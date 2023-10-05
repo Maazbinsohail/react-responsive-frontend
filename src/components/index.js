81a2fc42a9301ebc4ec8ee65272dff15b3d0d060
Maazbinsohail <maazbinsohail15@gmail.com>
2023-10-05 10:34:29 -0700

    add :initial files

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Clients from './Clients'
+import CTA from './CTA'
+import Business from './Business'
+import Hero from './Hero'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+import CardDeal from './CardDeal'
+import Billing from './Billing'
+
+export {
+    Navbar,
+    Footer,
+    Clients,
+    CTA,
+    Business,
+    Hero,
+    Stats,
+    Testimonials,
+    CardDeal,
+    Billing
+};
